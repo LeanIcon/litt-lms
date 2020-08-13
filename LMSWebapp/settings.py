c225f661bbcf02a077d7ba7a94b1293b07a26a97
Gloria Alabi <gloriaalabi9@gmail.com>
2020-08-13 10:48:17 -0700

    commit fixes

--- LMSWebapp/settings.py
+++ LMSWebapp/settings.py
@@ -88,8 +88,15 @@ WSGI_APPLICATION = 'LMSWebapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lmsdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -109,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    }
 ]
 
 
