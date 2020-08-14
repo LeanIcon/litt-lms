075fda025eae683e3d53299974e213b8e578fe6c
Gloria Alabi <gloriaalabi9@gmail.com>
2020-08-13 22:42:09 -0700

    api has been set up

---
+++ littapi/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Lms
+
+admin.site.register(Lms)
+
+# Register your models here.
---
+++ littapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LittapiConfig(AppConfig):
+    name = 'littapi'
