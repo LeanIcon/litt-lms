11a3c5d5c5d70e8910aae26eaac2b4e6c6457410
gloorya <gloriaalabi9@gmail.com>
2020-08-21 06:02:31 -0700

    Wrote a comment

--- starter/models.py
+++ starter/models.py
@@ -14,7 +14,7 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(max_length=150)
     signup_confirmation = models.BooleanField(default=False)
-
+    #Teaching myself how to code
 
     # company additiionals
     CompanyName = models.CharField(_('Company'), max_length=100, blank=True)
